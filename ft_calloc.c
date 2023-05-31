d37872e17536cddc4503e877e7e37f4914e2a727
lolo <lourdesruizvillar@gmail.com>
2023-05-31 16:49:53 +0200

    he quitado un include de calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,11 @@
 /*   By: mruiz-vi <mruiz-vi@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 12:06:39 by mruiz-vi          #+#    #+#             */
-/*   Updated: 2023/05/30 16:29:31 by mruiz-vi         ###   ########.fr       */
+/*   Updated: 2023/05/31 16:37:05 by mruiz-vi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
